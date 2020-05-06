22a45cee690d0c610d817407c10193bf55c906e0
armaganbayraktar01 <bayraktararmagan@gmail.com>
2020-05-06 15:52:38 +0300

    React APP created

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://create-react-app.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React App Created
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
